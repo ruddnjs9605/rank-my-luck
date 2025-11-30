5b5e7a02a16808e6ff8829c5257fb1b40a705cda
WON <rlaruddnjs9605@gmail.com>
2025-11-30 15:24:17 +0900

    Final update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import Login from "./pages/Login";
 import Nickname from "./pages/Nickname";
@@ -7,17 +7,21 @@ import Play from "./pages/Play";
 import Leaderboard from "./pages/Leaderboard";
 import History from "./pages/History";
 
+const DEFAULT_PATH = "/play";
+
 export default function App() {
   return (
     <>
       <TopBar />
       <div style={{ maxWidth: 480, margin: "0 auto", padding: "16px" }}>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Navigate to={DEFAULT_PATH} replace />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/nickname" element={<Nickname />} />
           <Route path="/play" element={<Play />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/history" element={<History />} />
+          <Route path="*" element={<Navigate to={DEFAULT_PATH} replace />} />
         </Routes>
       </div>
     </>
